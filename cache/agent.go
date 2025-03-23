2354c3568093ebc44ab6326a5cfd0861b6e09c96
advanced-go <mark.wilson.go@outlook.com>
2025-03-23 17:48:47 -0500

    Adding cache requests

--- cache/agent.go
+++ cache/agent.go
@@ -1,12 +1,18 @@
 package cache
 
 import (
+	"bytes"
+	"context"
 	"errors"
+	"fmt"
 	"github.com/behavioral-ai/collective/event"
+	"github.com/behavioral-ai/core/access"
 	"github.com/behavioral-ai/core/httpx"
 	"github.com/behavioral-ai/core/messaging"
 	"github.com/behavioral-ai/resiliency/common"
+	"io"
 	"net/http"
+	"net/url"
 	"time"
 )
 
@@ -57,16 +63,23 @@ func (a *agentT) Message(m *messaging.Message) {
 
 // Exchange - chainable exchange
 func (a *agentT) Exchange(next httpx.Exchange) httpx.Exchange {
-	return func(req *http.Request) (resp *http.Response, err error) {
+	return func(r *http.Request) (resp *http.Response, err error) {
 		if a.hostName == "" {
-			return &http.Response{StatusCode: http.StatusInternalServerError}, errors.New("cache host name is empty and not configured")
+			err = errors.New("cache host name is empty and not configured")
+			return &http.Response{StatusCode: http.StatusInternalServerError, Body: io.NopCloser(bytes.NewReader([]byte(err.Error())))}, err
 		}
-		// TODO: create request and send to the cache
-
+		url := newUrl(a.hostName, r.URL)
+		h := httpx.Copy(r.Header)
+		resp, err = a.get(url, h)
+		if resp.StatusCode == http.StatusOK {
+			resp.Header.Add(access.XCached, "true")
+			return resp, nil
+		}
 		if next != nil {
-			resp, err = next(req)
-		} else {
-			resp = &http.Response{StatusCode: http.StatusOK}
+			resp, err = next(r)
+			if resp.StatusCode == http.StatusOK {
+				go a.put(url, h, resp)
+			}
 		}
 		return
 	}
@@ -82,4 +95,44 @@ func (a *agentT) configure(m *messaging.Message) {
 		return
 	}
 	messaging.Reply(m, messaging.StatusOK(), a.Uri())
+}
+
+func (a *agentT) get(url string, h http.Header) (*http.Response, error) {
+	start := time.Now().UTC()
+	ctx, cancel := context.WithTimeout(context.Background(), a.timeout)
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return &http.Response{StatusCode: http.StatusInternalServerError, Body: io.NopCloser(bytes.NewReader([]byte(err.Error())))}, err
+	}
+	req.Header = h
+	resp, _ := httpx.Do(req)
+	reasonCode := ""
+	if resp.StatusCode == http.StatusGatewayTimeout {
+		reasonCode = access.ControllerTimeout
+	}
+	access.Log(access.EgressTraffic, start, time.Since(start), req, resp, access.Controller{Timeout: a.timeout, Code: reasonCode})
+	return resp, nil
+}
+
+func (a *agentT) put(url string, h http.Header, appResp *http.Response) {
+	start := time.Now().UTC()
+	ctx, cancel := context.WithTimeout(context.Background(), a.timeout)
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctx, http.MethodPut, url, appResp.Body)
+	if err != nil {
+		return
+	}
+	req.Header = h
+	resp, _ := httpx.Do(req)
+	reasonCode := ""
+	if resp.StatusCode == http.StatusGatewayTimeout {
+		reasonCode = access.ControllerTimeout
+	}
+	access.Log(access.EgressTraffic, start, time.Since(start), req, resp, access.Controller{Timeout: a.timeout, Code: reasonCode})
+	return
+}
+
+func newUrl(hostName string, url *url.URL) string {
+	return fmt.Sprintf("https://%v%v", hostName, url.String())
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.23.0
 
 require (
 	github.com/behavioral-ai/collective v0.0.0-20250321221549-e4e478559cd0
-	github.com/behavioral-ai/core v0.0.0-20250323142229-060874a46838
+	github.com/behavioral-ai/core v0.0.0-20250323220047-0e59b56faa01
 	golang.org/x/time v0.11.0
 )
 
