8f9f68ddf038b2f3252f74a38963e6dadfd607e3
advanced-go <mark.wilson.go@outlook.com>
2025-04-17 08:32:32 -0500

    Update root.go

--- endpoint/root.go
+++ endpoint/root.go
@@ -4,11 +4,9 @@ import (
 	"github.com/behavioral-ai/collective/exchange"
 	"github.com/behavioral-ai/core/host"
 	"github.com/behavioral-ai/core/httpx"
-	"github.com/behavioral-ai/core/messaging"
 	"github.com/behavioral-ai/core/rest"
 	"github.com/behavioral-ai/intermediary/cache"
 	"github.com/behavioral-ai/intermediary/cache/cachetest"
-	"github.com/behavioral-ai/intermediary/config"
 	"github.com/behavioral-ai/intermediary/routing"
 	"github.com/behavioral-ai/intermediary/routing/routingtest"
 	urn2 "github.com/behavioral-ai/intermediary/urn"
@@ -20,20 +18,20 @@ import (
 
 func newRootEndpoint() *rest.Endpoint {
 	// Overriding cache agent http exchange
-	_ = cache.Route
+	_ = cache.NamespaceName
 	cacheAgent := exchange.Agent(urn2.CacheAgent)
 	cacheAgent.Message(httpx.NewConfigExchangeMessage(cachetest.Exchange))
-	m := make(map[string]string)
-	m[config.CacheHostKey] = "localhost:8082"
-	cacheAgent.Message(messaging.NewConfigMapMessage(m))
+	//m := make(map[string]string)
+	//m[config.CacheHostKey] = "localhost:8082"
+	//cacheAgent.Message(messaging.NewConfigMapMessage(m))
 
 	// Overriding routing agent http exchange
-	_ = routing.Route
+	_ = routing.NamespaceName
 	routingAgent := exchange.Agent(urn2.RoutingAgent)
 	routingAgent.Message(httpx.NewConfigExchangeMessage(routingtest.Exchange))
-	m[config.AppHostKey] = "localhost:8080"
+	//m[config.AppHostKey] = "localhost:8080"
 	//m[config.TimeoutKey] = "10ms"
-	routingAgent.Message(messaging.NewConfigMapMessage(m))
+	//routingAgent.Message(messaging.NewConfigMapMessage(m))
 
 	_ = limiter.NamespaceName
 	_ = redirect.NamespaceName
